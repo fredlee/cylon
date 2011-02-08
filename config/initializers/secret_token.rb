e95cafb4e844b9b65c6e12c4c39d6df233f478a9
Fred Lee <lee.fred.1@gmail.com>
2011-02-08 14:49:25 -0600

    Creates an initial Rails 3.0.3 app.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cylon::Application.config.secret_token = 'deb9ba725a49eee51d41cb803f1e0914d7a667e4d5249fdc220dea6a5bf407d737845225daefd14f5c14d5a7259b2912a4fd69093570b421e7478ee5796e5178'
